fe228e87da5065fb983e4e49b3ba3a3f6e169fbb
sacifym <76045198+sacifym@users.noreply.github.com>
2023-03-24 16:30:19 +0100

    Add files via upload

--- Logiciel_test/Advanced.py
+++ Logiciel_test/Advanced.py
@@ -5,15 +5,7 @@ from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 
 # Chargement du fichier Excel
-wb = openpyxl.load_workbook('C:/Users/Maxence/Desktop/Logiciel_test/Inventaire.xlsx')
-
-# Accéder à la feuille "Utilisateurs"
-ws_users = wb['Utilisateurs']
-
-# Récupérer tous les noms d'utilisateurs dans la colonne A
-users = []
-for cell in ws_users['A']:
-    users.append(cell.value)
+wb = openpyxl.load_workbook('Inventaire.xlsx')
 
 # Accéder à la feuille "Critères"
 ws_criteria = wb['Critères']
@@ -24,6 +16,8 @@ for cell in ws_criteria['A']:
     criteria_set.add(cell.value)
 criterias = list(criteria_set)
 
+#Variable en read pour les subwindows
+selectedSerialNumber = "Should be overwritten"
 
 def is_available(item):
     # Open the workbook and select the sheet
@@ -40,11 +34,50 @@ def is_available(item):
                 return False
 
 
+class AnotherWindow2(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Information")
+        self.setGeometry(100, 100, 400, 200)
+
+        self.InputInitiales = QLineEdit()
+        self.InputInitiales.setPlaceholderText("Initiales")
+
+        self.InputLocalisation = QLineEdit()
+        self.InputLocalisation.setPlaceholderText("Localisation")
+
+        self.DateToday = QDate.currentDate()
+        self.DateToday = self.DateToday.toString("dd/MM/yyyy")
+
+        self.InputManip = QLineEdit()
+        self.InputManip.setPlaceholderText("Manip / stock")
+
+        self.ConfirmButton = QPushButton("Confirmer")
+        self.ConfirmButton.clicked.connect(self.ConfirmButtonFonction)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.InputInitiales)
+        layout.addWidget(self.InputLocalisation)
+        layout.addWidget(self.InputManip)
+        layout.addWidget(QLabel(self.DateToday))
+        layout.addWidget(self.ConfirmButton)
+
+        self.setLayout(layout)
+
+        self.show()
+
+    def ConfirmButtonFonction(self):
+        # Open the inventory workbook
+        workbook = openpyxl.load_workbook("Inventaire.xlsx")
+        sheet = workbook["Liste"]
+        print("Workbook opened")
+
+
 # Nouvelle fenetre créer quand on clique sur un objet dans la liste
 class AnotherWindow(QWidget):
     def __init__(self):
         super().__init__()
-        print("Window created")
 
         self.setWindowTitle("Information")
         self.setGeometry(100, 100, 500, 600)
@@ -52,17 +85,39 @@ class AnotherWindow(QWidget):
         self.PetitTableau.setRowCount(15)
         self.PetitTableau.setColumnCount(3)
 
+        # Bouton Test
+        self.Test = QPushButton("Test")
+        self.Test.clicked.connect(self.TestFonction)
+
+        # Bouton "Modifier"
+        self.Modifier = QPushButton("Modifier")
+        self.Modifier.clicked.connect(self.ModifierFonction)
+
         # Appel de la fonction qui copie le tableau excel dans le tableau de la fenetre
         self.copyExcelTable()
 
         layout = QVBoxLayout()
         layout.addWidget(self.PetitTableau)
+        layout.addWidget(self.Modifier)
+        layout.addWidget(self.Test)
+
         self.setLayout(layout)
 
         self.show()
 
+    def TestFonction(self):
+        print("Selected serial number in another class", selectedSerialNumber)
+
+
+    def ModifierFonction(self):
+        # Create a new window
+        self.w = AnotherWindow2()
+        self.w.show()
+        print(selectedSerialNumber)
+
     # Fonction qui copie un tableau excel dans un tableau de la fenetre
     def copyExcelTable(self):
+
         # Open the workbook and select the sheet
         workbook = openpyxl.load_workbook("test.xlsx", data_only=True)
         sheet = workbook["Résumé"]
@@ -75,10 +130,8 @@ class AnotherWindow(QWidget):
             for col in range(1, 4):
                 # Copie de la cellule dans le tableau de la fenetre
                 self.PetitTableau.setItem(row - 1, col - 1, QTableWidgetItem(str(sheet.cell(row, col).value)))
-                # self.PetitTableau.setItem(row - 1, col - 1, QTableWidgetItem(sheet.cell(row, col).value))
 
 
-# Subclass QMainWindow to customize your application's main window
 class MainWindow(QMainWindow):
 
     def __init__(self):
@@ -121,6 +174,16 @@ class MainWindow(QMainWindow):
         # to take up all the space in the window by default.
         self.setCentralWidget(widget)
 
+        # Vérification de la version du logiciel
+        wb = openpyxl.load_workbook('Inventaire.xlsx')
+        sh_ver = wb['Version']
+        version = sh_ver['A2'].value
+        if version != "Alpha 1.0":
+            print("Version du logiciel incompatible")
+            QMessageBox.critical(None, "Erreur",
+                                 "Version du logiciel incompatible, Merci de télécharger la dernière version")
+            sys.exit()
+
     def SearchSerialNumber(self, serial_number):
         # Open the workbook and select the sheet
         workbook = openpyxl.load_workbook("Inventaire.xlsx")
@@ -131,20 +194,14 @@ class MainWindow(QMainWindow):
 
         # Loop through the rows to find the matching item even partially
         for row in sheet.iter_rows(min_row=2, values_only=True):
-            # any serial number starting with the same letters as the search bar
+            # if the serial number in search bar is in the row, it will add the item to the listview
             if str(serial_number).lower() in str(row[3]).lower():
+                # Add the item to the listview
                 self.model.appendRow(QStandardItem(str(row[1])))
-                #Applying the color function
+                # Applying the color function
                 self.color(self.model.item(0))
-            # if the viewlist is empty, it will display "Aucun résultat"
-            elif self.model.rowCount() == 0:
-                self.model.appendRow(QStandardItem("Aucun résultat"))
-            # if it doesnt include the number, it will delete the row
-            elif str(serial_number).lower() not in str(row[3]).lower():
-                self.model.removeRow(0)
-            # if the search bar is empty, it will display nothing
-            elif str(serial_number).lower() == "":
-                self.model.clear()
+            else:  # if the serial number is not in the row, it will not add the item to the listview
+                pass
 
     # fonction qui choisi une couleur en fonction de la disponibilité
     def color(self, item):
@@ -178,20 +235,35 @@ class MainWindow(QMainWindow):
 
     # Create a new window
     def show_new_window(self, checked):
-        print("Creating new window...")
         self.w = AnotherWindow()
         self.w.show()
+
+    def AssociateSerialNumber(self, AssociateSerialNumber):
+        # Open the workbook and select the sheet
+        global selectedSerialNumber
+        workbook = openpyxl.load_workbook("Inventaire.xlsx")
+        sheet = workbook["Liste"]
+
+        # Associate the serial number of the clicked item to a variable
+        for row in sheet.iter_rows(min_row=2, values_only=True):
+            if row[1] == self.listView.currentIndex().data():
+                AssociateSerialNumber = row[3]
+                print("globally selected serial number before", selectedSerialNumber)
+                selectedSerialNumber = AssociateSerialNumber
+                print("globally selected serial number after", selectedSerialNumber)
 
     def ClickOnObject(self):
         # Open the workbook and select the sheet
         workbook = openpyxl.load_workbook("Inventaire.xlsx")
         sheet = workbook["Liste"]
+        AssociateSerialNumber = ''
 
-        # Get information in row 4
+        # Call he function to associate the serial number of the clicked item to a variable
+        self.AssociateSerialNumber(AssociateSerialNumber)
+
+        # Open the new window
         for row in sheet.iter_rows(min_row=2, values_only=True):
             if row[1] == self.listView.currentIndex().data():
-                # Create a MessageBox
-                # QMessageBox.about(self, "Coefficient", "Coefficient : " + str(row[3]))
                 # Create a new window
                 self.show_new_window(True)
 
